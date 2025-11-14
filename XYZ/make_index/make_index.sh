846c1e9b3c1fe4bda65cd980686abd5dcaf8e3ab
marcos.caa1 <marcos.caa@gmail.com>
2025-11-14 10:59:36 -0800

    make index xyz

---
+++ XYZ/make_index/make_index.sh
@@ -0,0 +1,22 @@
+function make_index(){
+  traj=$1
+  #Make index file
+  nat=`head -n 1 $traj | awk '{print $1}'`
+  nframes=`grep Lattice $traj | wc -l | awk '{print $1}'`
+  nattype=`head -n $((nat+2)) $traj | awk 'BEGIN{c=0} ($1=="N"){c+=1} END{print c}'`
+  nequil=$((nframes/10))
+  stride=1
+  nbins=300
+  rcut="6.0"
+
+  cat << EOF > index_zdf
+$nat $nframes $nequil $stride $nbins 3 
+0.0 
+EOF
+
+  #echo "${nat} ${nattype} ${nframes} 0 0.10 1 10000" > index_msd
+  #echo "${nat} ${nframes} ${nequil} ${stride}" > index_hb
+
+}
+
+make_index $1
